6eaccebf570098f7eaf0fdf39854973d6ef1c977
orange-buffalo <orange-buffalo@users.noreply.github.com>
2026-03-27 15:14:18 +1100

    noth

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -15,43 +15,43 @@ object Build : BuildType({
         root(DslContext.settingsRoot)
     }
 
-    steps {
-        script {
-            name = "Install SDKMAN and init Java"
-            scriptContent = """
-                #!/usr/bin/env bash
-                set -e
-
-                # Install SDKMAN if not already present
-                if [ ! -d "${'$'}HOME/.sdkman" ]; then
-                    curl -s "https://get.sdkman.io" | bash
-                fi
-
-                # Load SDKMAN
-                export SDKMAN_DIR="${'$'}HOME/.sdkman"
-                source "${'$'}HOME/.sdkman/bin/sdkman-init.sh"
-
-                # Install and use Java version from .sdkmanrc
-                sdk env install
-                sdk env
-            """.trimIndent()
-        }
-        script {
-            name = "Maven clean install"
-            scriptContent = """
-                #!/usr/bin/env bash
-                set -e
-
-                export SDKMAN_DIR="${'$'}HOME/.sdkman"
-                source "${'$'}HOME/.sdkman/bin/sdkman-init.sh"
-                sdk env
-
-                ./mvnw clean install
-            """.trimIndent()
-        }
-    }
-
-    requirements {
-        exists("env.HOME")
-    }
+//    steps {
+//        script {
+//            name = "Install SDKMAN and init Java"
+//            scriptContent = """
+//                #!/usr/bin/env bash
+//                set -e
+//
+//                # Install SDKMAN if not already present
+//                if [ ! -d "${'$'}HOME/.sdkman" ]; then
+//                    curl -s "https://get.sdkman.io" | bash
+//                fi
+//
+//                # Load SDKMAN
+//                export SDKMAN_DIR="${'$'}HOME/.sdkman"
+//                source "${'$'}HOME/.sdkman/bin/sdkman-init.sh"
+//
+//                # Install and use Java version from .sdkmanrc
+//                sdk env install
+//                sdk env
+//            """.trimIndent()
+//        }
+//        script {
+//            name = "Maven clean install"
+//            scriptContent = """
+//                #!/usr/bin/env bash
+//                set -e
+//
+//                export SDKMAN_DIR="${'$'}HOME/.sdkman"
+//                source "${'$'}HOME/.sdkman/bin/sdkman-init.sh"
+//                sdk env
+//
+//                ./mvnw clean install
+//            """.trimIndent()
+//        }
+//    }
+//
+//    requirements {
+//        exists("env.HOME")
+//    }
 })
